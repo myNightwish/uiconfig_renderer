9b7c9024279e6021fcb69a4d171a3d785d442643
zhoumaoling <zhoumaoling@baidu.com>
2024-07-07 18:25:21 +0800

    🎉初始化正常运行

--- examples/play.js
+++ examples/play.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './play.vue';
 import router from './router.js';
 import MiUI from 'mi-element-ui';
+import 'mi-element-ui/lib/theme-chalk/index.css';
 
 Vue.use(MiUI);
 
